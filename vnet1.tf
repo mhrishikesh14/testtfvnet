91108b9745e525d80a432c8a6cd67396a736ee54
mhrishikesh14 <100055427+mhrishikesh14@users.noreply.github.com>
2022-05-18 19:06:27 +0530

    Update vnet1.tf

--- vnet1.tf
+++ vnet1.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "example" {
 }
 resource "azurerm_virtual_network" "example" {
   name                = "vnet1"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4","10.0.0.4"]
 
